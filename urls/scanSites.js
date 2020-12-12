e64b5244e749c7bee56b2adc2e2dd767c0fb82a7
Marco <30555419+emdant@users.noreply.github.com>
2020-12-12 18:23:24 +0100

    Started url threat analyses

--- urls/scanSites.js
+++ urls/scanSites.js
@@ -1,31 +1,114 @@
 const axios = require('axios');
 const fs = require('fs');
 const urlsList = require('./urls.json');
+const FormData = require('form-data');
 require('dotenv').config();
 
-//    Only for DEBUG
-/*process.stdin.resume();
+process.stdin.resume();
 process.on('SIGINT', function() {
   saveData();
   process.exit();
 });
-*/
 
-// Global variables
-const virus_total_key = process.env.VIRUS_TOTAL_API_KEY;
-
-// Prebuilt istance for axios get calls
-const instance = axios.create({
-    baseURL: 'https://www.virustotal.com/api/v3/',
-    headers: { 'x-apikey': virus_total_key }
-});
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const saveData = () => {
     fs.writeFileSync('./urls/urls.json', JSON.stringify(urlsList, null, 2), {encoding: 'utf-8'});
 };
 
-const analyze = (url) => {
-    // Gestire il problema della quota
-    // Prima chiamata /urls per l'id (POST: url: url)
-    // Seconda chiamata /analyses/{id} (GET)
+const computeThreat = (stats) => {
+    const total = Object.keys(stats).reduce((total, key) => {
+        return total += stats[key];
+    }, 0);
+
+    if (total === 0)
+        return 'Undetected';
+    
+    const rates = [stats.harmless, stats.malicious, stats.suspicious].map(el => el / total);
+    const max = Math.max(...rates); 
+    if (max === 0) 
+        return 'Undetected';
+
+    switch (rates.indexOf(max)) {
+        case 0: return 'Harmless';
+        case 1: return 'Malicious';
+        case 2: return 'Suspicious';
+    }
+}
+
+// Global variables
+const virus_total_key = process.env.VIRUS_TOTAL_API_KEY;
+
+const analysesIds = {};
+
+const analyze = async (url) => {
+
+    try {
+        let analysesId = analysesIds[url];
+
+        if (analysesId == undefined) {
+
+            const formData = new FormData();
+            formData.append('url', url);
+
+            // chiamata all'endpoint /urls che analizza l'url
+            const urlsResponse = await axios.post('https://www.virustotal.com/api/v3/urls', 
+                formData, 
+                { headers: formData.getHeaders({'x-apikey': virus_total_key}) }
+            );
+
+            analysesId = urlsResponse.data.data.id;
+            console.log(`Analyses id for ${url}: ${analysesId}`);
+        }
+
+        await sleep(3500);
+        const analysesResponse = await axios.get(`https://www.virustotal.com/api/v3/analyses/${analysesId}`, 
+            {headers: { 'x-apikey': virus_total_key }}
+        );
+
+        const analyses = analysesResponse.data.data;
+
+        if (analyses.attributes.status === 'completed') {
+            return computeThreat(analyses.attributes.stats);
+        } else {
+            console.log(`Analyses for ${url} not completed, waiting...`);
+            await sleep(1500);
+            analysesId[url] = analysesId;
+            return await analyze(url);
+        }
+
+
+    } catch (err) {
+        if (err.response) {
+            const error = err.response.data.error;
+            if (error.code === 'QuotaExceededError') {
+                console.log(`Error ${error.code}: ${error.message}`);
+                await sleep(20000);
+                return await analyze(url);
+            } else {
+                throw error;
+            }
+        } else {
+            throw err;
+        }
+    }
 };
+
+const main = async () => {
+    for (let site of urlsList) {
+        if (site.rating === null) {
+            site.rating = await analyze(site.url);
+        }
+    }    
+}
+
+main()
+    .then(() => {
+        console.log('All analyses completed');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .finally(() => {
+        saveData();
+    });
